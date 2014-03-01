1f6fce2beebb964cd03413e56920dca4d61394c0
Dombot2 <dom.bot@mail.com>
2014-03-01 15:48:55 +0100

    With Spanish

--- election.py
+++ election.py
@@ -154,17 +154,23 @@ def processCategory(wikipediaSource, template, categoryname, fields, valuesConst
               hr.procesPage(site, page)
                                             
                                             
+templateName =  dict()
+templateName["en"] = u"Infobox election"
+templateName["es"] = u"Ficha de elección"
 
 fields = dict()
 valuesConstraints = dict()
-fields["country"] = "[[P17]]"
+fields[u"country"] = "[[P17]]"
+fields[u"país"] = "[[P17]]"
 valuesConstraints["[[P17]]"] = set(["Q889", "Q222", "Q262", "Q228", "Q916", "Q781", "Q414", "Q399", "Q408", "Q40", "Q227", "Q778", "Q398", "Q902", "Q244", "Q184", "Q31", "Q242", "Q962", "Q917", "Q750", "Q225", "Q963", "Q155", "Q921", "Q219", "Q965", "Q967", "Q424", "Q1009", "Q16", "Q1011", "Q929", "Q657", "Q298", "Q148", "Q739", "Q970", "Q800", "Q1008", "Q224", "Q241", "Q229", "Q213", "Q33946", "Q974", "Q756617", "Q977", "Q784", "Q786", "Q574", "Q736", "Q79", "Q792", "Q983", "Q986", "Q191", "Q115", "Q702", "Q712", "Q33", "Q142", "Q1000", "Q1005", "Q230", "Q183", "Q117", "Q41", "Q769", "Q774", "Q1006", "Q1007", "Q734", "Q790", "Q783", "Q28", "Q189", "Q668", "Q252", "Q794", "Q796", "Q27", "Q801", "Q38", "Q766", "Q17", "Q810", "Q232", "Q114", "Q710", "Q817", "Q813", "Q819", "Q211", "Q822", "Q1013", "Q1014", "Q1016", "Q347", "Q37", "Q32", "Q221", "Q1019", "Q1020", "Q833", "Q826", "Q912", "Q233", "Q709", "Q1025", "Q1027", "Q96", "Q217", "Q711", "Q236", "Q235", "Q1028", "Q1029", "Q836", "Q1030", "Q697", "Q837", "Q29999", "Q664", "Q811", "Q1032", "Q1033", "Q423", "Q20", "Q842", "Q843", "Q695", "Q804", "Q691", "Q733", "Q419", "Q928", "Q36", "Q45", "Q846", "Q971", "Q218", "Q159", "Q1037", "Q763", "Q760", "Q757", "Q683", "Q238", "Q1039", "Q851", "Q1041", "Q403", "Q1042", "Q1044", "Q334", "Q214", "Q215", "Q685", "Q1045", "Q258", "Q884", "Q958", "Q29", "Q854", "Q1049", "Q730", "Q1050", "Q34", "Q39", "Q858", "Q863", "Q924", "Q869", "Q945", "Q678", "Q754", "Q948", "Q43", "Q874", "Q672", "Q1036", "Q212", "Q878", "Q145", "Q30", "Q77", "Q265", "Q686", "Q237", "Q717", "Q881", "Q180573", "Q805", "Q36704", "Q953", "Q954", "Q28513", "Q2895", "Q618399", "Q179876", "Q5291089", "Q70972", "Q7735661", "Q41304""7318", "Q713750", "Q16957", "Q5555325", "Q161885", "Q5622720", "Q156418", "Q223936", "Q172579", "Q6392538", "Q6741448", "Q7603765", "Q14759030", "Q12560", "Q2006542", "Q211274", "Q27306", "Q34266", "Q2184", "Q230791", "Q14920623", "Q193619", "Q15180", "Q170588", "Q7842409", "Q7877575", "Q174193", "Q191077", "Q723118", "Q43287", "Q12548", "Q1747689", "Q844930", "Q2429397", "Q23334", "Q865", "Q1246", "Q407199", "Q6250", "Q55", "Q21203", "Q25279", "Q26273", "Q35", "Q223", "Q4628", "Q34020", "Q33788"])
-  
+
 #fields["previous_election"] = "[[P155]]"
 #fields["next_election"] = "[[P156]]"
 #fields["type"] = "[[P173]]"
 valuesConstraints["[[P173]]"] = set(["Q858439", "Q1076105", "Q152450", "Q669262", "Q1128324"])
 fields["after_election"] = "[[P991]]"
+fields["sucesor"] = "[[P991]]"
 #fields["election_date"] = "[[P585]]" #Pas de traitement des dates pour le moment
 
-processCategory( u"en", u"Infobox election", sys.argv[1], fields, valuesConstraints)
+#processCategory( u"en", u"Infobox election", u"Presidential elections in Romania", fields, valuesConstraints)
+processCategory( sys.argv[2], templateName[sys.argv[2]], sys.argv[1], fields, valuesConstraints)
